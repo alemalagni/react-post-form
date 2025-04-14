3d2571322ef0c30a9b97cf1da449f37c7e514d6c
alemalagni <alemalagni@gmail.com>
2025-04-14 14:58:51 +0200

    feat(app.js): add script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import './App.css'
 
 function App() {
-
+    <>
+        <p>Nuovo post</p>
+        <div>
+            <p>Inserire il nome dell'autore:</p>
+            <input type="text" id='inputAuthor' />
+        </div>
+        <div>
+            <p>Inserire il titolo del post:</p>
+            <input type="text" id='inputTitle' />
+        </div>
+        <div>
+            <p>Inserire il corpo del post:</p>
+            <input type="text" id='inputBody' />
+        </div>
+        <div>
+            <select name="" id="inputPublic">
+                <option value="true">Pubblica</option>
+                <option value="false">Salva come bozza</option>
+            </select>
+        </div>
+    </>
 }
 
 export default App
