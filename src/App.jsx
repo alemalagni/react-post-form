2a9e3842a59d15fedd666ce781e2229ce0cedd9a
alemalagni <alemalagni@gmail.com>
2025-04-14 15:54:42 +0200

    feat(app.js): add formData

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
+import { useState } from 'react';
 import './App.css'
 
 export default function App() {
+    const [formData, setFormData] = useState({
+        inputAuthor: "",
+        inputTitle: "",
+        inputBody: "",
+        inputPublic: true,
+    })
+
     return (<>
         <h2>Nuovo post</h2>
         <div>
             <p>Inserire il nome dell'autore:</p>
         </div>
         <div>
-            <input type="text" id='inputAuthor' />
+            <input
+                type="text"
+                name='inputAuthor'
+                value={formData.inputAuthor}
+            />
         </div>
         <div>
             <p>Inserire il titolo del post:</p>
         </div>
         <div>
-            <input type="text" id='inputTitle' />
+            <input
+                type="text"
+                name='inputTitle'
+                value={formData.inputTitle}
+            />
         </div>
-
         <div>
             <p>Inserire il corpo del post:</p>
         </div>
         <div>
-            <input type="text" id='inputBody' />
+            <input
+                type="text"
+                name='inputBody'
+                value={formData.inputBody}
+            />
         </div>
 
         <div>
-            <select name="" id="inputPublic">
+            <select
+                name="inputPublic"
+                value={formData.inputPublic}
+            >
                 <option value="true">Pubblica</option>
                 <option value="false">Salva come bozza</option>
             </select>
